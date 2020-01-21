6bac306024ad9f02a542a20ec6ea4bf115b52649
Oleg Kachanov <ovkachanov1988@gmail.com>
2020-01-21 21:34:55 +0300

    fix 4.rb

--- lesson_1/4.rb
+++ lesson_1/4.rb
@@ -8,9 +8,10 @@ c = gets.chomp.to_i
 d = b**2 - 4 * a * c
 
 if d > 0
-  х1 = (-b + Math.sqrt(d))/(2 * a)
-  x2 = (-b - Math.sqrt(d))/(2 * a)
-  puts "Корни: x1 = #{х1}, x2 = #{x2}"
+  d_root = Math.sqrt(d)
+  x1 = (-b + d_root) / (2 * a)
+  x2 = (-b - d_root) / (2 * a)
+  puts "Корни: x1 = #{x1}, x2 = #{x2}"
 elsif d == 0
   x1 = -b/2 * a
   puts "Уравнение имеет один корень равный #{x1}"
